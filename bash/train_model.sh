1b25d1cfab6421be9e70b05386e454e83a43b3aa
Giuseppe Attanasio <giuseppeattanasio6@gmail.com>
2021-07-01 13:30:54 +0200

    Minor changes

--- bash/train_model.sh
+++ bash/train_model.sh
@@ -10,6 +10,7 @@ for year in {2007..2017}; do
             --do_grid_search \
             --normalize
 
+    done
 
     for model in RFC; do
 
@@ -17,5 +18,7 @@ for year in {2007..2017}; do
             out_$year_$model \
             --training_type year \
             --do_grid_search
+
+    done
 
 done
