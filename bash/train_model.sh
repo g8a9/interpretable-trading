49075db4c4446b280b55a44e95eaabccaccedae4
Giuseppe Attanasio <giuseppeattanasio6@gmail.com>
2021-07-01 13:35:53 +0200

    Minor changes

--- bash/train_model.sh
+++ bash/train_model.sh
@@ -5,9 +5,9 @@ for year in {2007..2017}; do
     for model in SVC KNN LG; do
 
         python src/models/train_model.py \
-            out_$year_$model \
-            $model \
-            $year \
+            out_${year}_${model} \
+            ${model} \
+            ${year} \
             --training_type year \
             --do_grid_search \
             --normalize
@@ -17,9 +17,9 @@ for year in {2007..2017}; do
     for model in RFC; do
 
         python src/models/train_model.py \
-            out_$year_$model \
-            $model \
-            $year \
+            out_${year}_${model} \
+            ${model} \
+            ${year} \
             --training_type year \
             --do_grid_search
 
