64dbd3a0cd58d7f44620fafec0ed4c8a9e1f8418
Giuseppe Attanasio <giuseppeattanasio6@gmail.com>
2021-08-23 19:31:16 +0200

    Add LSTM runner

--- bash/train_LSTM.sh
+++ bash/train_LSTM.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-model=$1
+model=LSTM
 
 for year in {2007..2017}; do
 
@@ -11,10 +11,15 @@ for year in {2007..2017}; do
             ${model} \
             ${year} \
             --training_type year \
-            --do_grid_search \
             --normalize \
             --seed $seed \
-            --log_comet
+            --log_comet \
+            --seq_length 10 \
+            --batch_size 1024 \
+            --max_epochs 100 \
+            --lr 2e-5 \
+            --early_stop 10 \
+            --gpus 1
 
     done
 
---
+++ bash/train_MLP_10seeds.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+model=MLP
+
+for year in {2007..2017}; do
+
+    for seed in {0..9}; do
+
+        python src/models/train_model.py \
+            output/year/out_${year}_${model}_${seed} \
+            ${model} \
+            ${year} \
+            --training_type year \
+            --do_grid_search \
+            --normalize \
+            --seed $seed \
+            --log_comet
+
+    done
+
+
+done
